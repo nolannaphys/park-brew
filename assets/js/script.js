db85b4e766acca4114a1a63b5142ce05ede059e0
Billy Cody <william.cody1015@gmail.com>
2023-04-25 21:17:40 -0400

    added html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
 // API key for National Parks Database
-var apiKey='nsqq7nIIHbeaGT4DasDU3QLDbqazbcJTW8zA7SWb'
-var searchValue=$('#searchInput')
+var apiKey = 'nsqq7nIIHbeaGT4DasDU3QLDbqazbcJTW8zA7SWb'
+var cityValue = $('#cityInput')
 
+// this will define the results container to connect with html
+var resultsContainer = document.querySelector("#brewery-results")
 // JS for Brewery API
-$("#searchButton").on("click",function(event){
-    var searchValue=$("#searchInput").val()
-    console.log (searchValue)
-    // geoCode(searchValue)
+$("#citySearchButton").on("click", function (event) {
+    var cityValue = $("#cityInput").val()
+    resultsContainer.textContent = cityValue
+    brewery(cityValue)
 })
-function brewery(searchValue){
-    fetch('https://api.openbrewerydb.org/v1/breweries?by_city=${searchValue}&per_page=3')
+// get values from brewery API once the city is entered and button is clicked
+function brewery(cityValue) {
+    fetch(`https://api.openbrewerydb.org/v1/breweries?by_city=${cityValue}&per_page=3`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+
+        })
 }
+
